e383d0182883ac9cc92c08eee71a9b1fe161cc29
BinqiangLiu <134038370+BinqiangLiu@users.noreply.github.com>
2023-10-27 16:04:34 +0800

    Update app.py

--- app.py
+++ app.py
@@ -21,6 +21,6 @@ user_query = st.text_input("Enter your query here:")
 with st.spinner("AI Thinking...Please wait a while to Cheers!"):    
     if user_query !="" and not user_query.strip().isspace() and not user_query == "" and not user_query.strip() == "" and not user_query.isspace():
         response = call_chatbot_api(user_query)
-        st.write("AI Response:")
+        st.write("USino AI Response:")
         st.write(response)
         print(response)  # 打印Chatbot的响应
